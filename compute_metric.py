31dd3f04dc23443efbec0fa8dfd67b4a419a9786
Afroz Alam <afrozalam@microsoft.com>
2023-02-21 13:03:55 -0800

    adding computer metric

---
+++ compute_metric.py
@@ -0,0 +1,46 @@
+from sklearn.metrics import classification_report
+from datasets import load_dataset
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+
+
+""""
+ False       1.00      1.00      1.00      1882
+        True       0.97      0.99      0.98       240
+
+    accuracy                           1.00      2122
+   macro avg       0.98      0.99      0.99      2122
+weighted avg       1.00      1.00      1.00      2122
+"""
+
+tokenizer = AutoTokenizer.from_pretrained(
+    "./authenticheck")
+model = AutoModelForSequenceClassification.from_pretrained(
+    "./authenticheck")
+
+y_true = []
+y_pred = []
+
+
+def predict_func(text: str):
+    inputs = tokenizer(text, return_tensors='pt',
+                       max_length=512, truncation=True)
+    outputs = model(**inputs)
+    scores = outputs.logits[0].softmax(0).detach().numpy()
+    result = {"label": scores.argmax().item(), "score": scores.max().item()}
+    return result
+
+
+def predict(row):
+    print("predicting for text: ", row['answer'])
+    text = row['answer']
+    y_true.append(row['chatgpt'])
+    res = predict_func(text)
+    pred = True if res['label'] == 1 else False
+    y_pred.append(pred)
+
+
+dataset = load_dataset("datasets",  data_files={
+    "train": "train.jsonl", "test": "test.jsonl"})
+dataset["test"].map(predict, batched=False)
+
+print(classification_report(y_true, y_pred))
--- predict.py
+++ predict.py
@@ -1,8 +1,4 @@
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
-from datasets import load_dataset
-from transformers import TrainingArguments, Trainer
-import numpy as np
-import evaluate
 
 tokenizer = AutoTokenizer.from_pretrained(
     "./authenticheck")
@@ -14,7 +10,6 @@ def predict_func(text: str):
     inputs = tokenizer(text, return_tensors='pt',
                        max_length=512, truncation=True)
     outputs = model(**inputs)
-    print(outputs.logits)
     scores = outputs.logits[0].softmax(0).detach().numpy()
     result = {"label": scores.argmax().item(), "score": scores.max().item()}
     return result
